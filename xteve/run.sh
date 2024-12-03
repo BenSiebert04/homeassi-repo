7aabd00af0a63c993f7d2969b8c7731a0a2c0ba3
Bsie0 <112330672+Bsie0@users.noreply.github.com>
2024-12-03 15:35:10 +0100

    Test

--- xteve/run.sh
+++ xteve/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# EULA akzeptieren
-if [ "${EULA}" != "true" ]; then
-    echo "You need to accept the EULA by setting 'eula' to true in the options."
-    exit 1
-fi
 
 # Minecraft Server starten
 java -Xmx=8192M -Xms=1024M -jar minecraft_server.jar nogui
